456e470a7114eb3b04f48413d21780f902f29017
Dima <dima.tsymbal12345@gmail.com>
2021-11-13 12:19:39 +0200

    home work done

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ button.addEventListener('click', () => {
     const inputColor = document.getElementById('color')
 
     if(select === 'square') {
-        figure.classList.remove('triangle', 'circle')
+        figure.classList.remove('rectangle', 'circle')
         figure.classList.toggle('square')
         
         figure.style.backgroundColor = inputColor.value
-    } else if(select === 'triangle') {
+    } else if(select === 'rectangle') {
        
         figure.classList.remove('square', 'circle')
-        figure.classList.toggle('triangle')
-        figure.style.borderBottom = `200px solid ${inputColor.value}`
-        figure.style.backgroundColor = 'white'
+        figure.classList.toggle('rectangle')
+        figure.style.backgroundColor = inputColor.value
     } else if(select === 'circle') {
-        figure.classList.remove('square', 'triangle')
+        figure.classList.remove('square', 'rectangle')
         figure.classList.toggle('circle')
         
         figure.style.backgroundColor = inputColor.value
